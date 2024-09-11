a6001946252c56d0c8773c721445ab0749362ff3
Amerkowitz <amerkowi@hamilton.edu>
2024-09-11 16:24:11 +0000

    description of changes

---
+++ in class activity 2.R
@@ -0,0 +1,25 @@
+# in class activity
+# question 1
+snowflake <- c(2.5,3,5,4.5)
+snowflake.cm<-snowflake*2.54
+
+# question 2
+
+peaknames <- c("Mount Haystack", "Mount Skylight", "Dix Mountain", "Gray Peak")
+# elevation in feet
+elevation <- c(4960,4926,4857, 4840)
+#ascent in feet
+ascent <- c(3570, 4265,2800,4178)
+#round trip length in miles
+length <- c(17.8,17.9, 13.2, 16)
+highPeaks <- data.frame(peakNames = peakNames,
+                        elevation = elevation,
+                        ascent = ascent,
+                        length = length)
+# question 3
+44*9/5+32
+0*9/5+32
+20*9/5+32
+35*9/5+32
+
+
